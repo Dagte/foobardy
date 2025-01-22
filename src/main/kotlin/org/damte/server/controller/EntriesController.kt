710e65646fbb949b058bb008067a85711bad9dcb
Dagte_ <62153835+Dagte@users.noreply.github.com>
2025-01-22 23:16:21 +0200

    Makes POST /entries update content

--- src/main/kotlin/org/damte/server/controller/EntriesController.kt
+++ src/main/kotlin/org/damte/server/controller/EntriesController.kt
@@ -4,13 +4,9 @@ import io.ktor.http.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.buildJsonObject
 import org.damte.org.damte.server.StorageManager
-import org.damte.org.damte.server.model.DailyEntry
 import org.damte.org.damte.server.model.request.DailyEntryRequest
 import org.damte.org.damte.server.util.toDailyEntry
 import org.koin.ktor.ext.inject
@@ -23,27 +19,19 @@ fun Route.entriesRoutes() {
 
 
     route("/entries") {
-        // Get all entries
         get {
             val entries = storageManager.loadEntries()
             call.respond(entries)
         }
 
-        // Create a new entry
         post {
-            val contentType = call.request.contentType()
-            println("Received Content-Type: $contentType")
             val bodyText = call.receiveText()
-            println("Received body: $bodyText")
-
             try {
                 val entry = json.decodeFromString<DailyEntryRequest>(bodyText)
-                println("Parsed entry: $entry")
-
-                val createdEntry = storageManager.saveEntries(listOf(entry.toDailyEntry()))
-                call.respond(HttpStatusCode.Created, createdEntry)
+                val result = storageManager.updateEntry(entry.toDailyEntry())
+                call.respond(HttpStatusCode.Created, result)
             } catch (e: SerializationException) {
-                println("Error parsing JSON: ${e.message}")
+                ("Error parsing JSON: ${e.message}")
                 call.respond(HttpStatusCode.BadRequest, "Invalid JSON format")
             }
         }
